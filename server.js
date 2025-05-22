e0cc0b782f572ce1df8ea79f30526e429fcce204
choddenbach <corey@aeroservicesut.com>
2025-05-22 07:32:52 -0600

    proxy doc types

--- server.js
+++ server.js
@@ -1,10 +1,43 @@
 const http = require('http');
+const https = require('https');
 const fs = require('fs');
 const path = require('path');
+
+// FAA API key is also hard coded in the frontend for this demo
+const FAA_DRS_API_KEY = 'c480cd075825a9f44beab596ba0cada217476801';
 
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
+  if (req.url === '/api/doc-types') {
+    const options = {
+      hostname: 'drs.faa.gov',
+      path: '/api/doc-types',
+      headers: { 'x-api-key': FAA_DRS_API_KEY }
+    };
+    https
+      .get(options, drsRes => {
+        let data = '';
+        drsRes.on('data', chunk => {
+          data += chunk;
+        });
+        drsRes.on('end', () => {
+          res.writeHead(200, {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+          });
+          res.end(data);
+        });
+      })
+      .on('error', err => {
+        res.writeHead(500, {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*'
+        });
+        res.end(JSON.stringify({ error: err.message }));
+      });
+    return;
+  }
   let filePath = '.' + req.url;
   if (filePath === './') {
     filePath = './index.html';
